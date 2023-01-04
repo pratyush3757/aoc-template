07b2ad36780730dc3cf79afeecfe475b6258a8f2
Pro3757 <pratyush3757@gmail.com>
2023-01-04 19:56:49 +0530

    fix project name in benchmarks

--- solutions/rust/{{year}}/day_{{day}}/benches/bench.rs
+++ solutions/rust/{{year}}/day_{{day}}/benches/bench.rs
@@ -7,14 +7,14 @@ use criterion::Criterion;
 fn part_one_benchmark(c: &mut Criterion) {
 	c.bench_function("{{year}} day {{day}} part one", |b| {
 		let input = aoclib::reader({{year}}, {{day}}, "input.txt").unwrap();
-		b.iter(|| aoc{{year}}{{day}}::PartOne::solve(&input).unwrap())
+		b.iter(|| aoc_{{year}}_{{day}}::PartOne::solve(&input).unwrap())
 	});
 }
 
 fn part_two_benchmark(c: &mut Criterion) {
 	c.bench_function("{{year}} day {{day}} part two", |b| {
 		let input = aoclib::reader({{year}}, {{day}}, "input.txt").unwrap();
-		b.iter(|| aoc{{year}}{{day}}::PartTwo::solve(&input).unwrap())
+		b.iter(|| aoc_{{year}}_{{day}}::PartTwo::solve(&input).unwrap())
 	});
 }
 
